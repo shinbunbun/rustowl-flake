a930d59064d90525ff409e8eeca10ecd0584febb
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-04-09 09:42:21 +0900

    fix

--- nix/package.nix
+++ nix/package.nix
@@ -13,11 +13,11 @@
 in
   rustPlatform.buildRustPackage rec {
     pname = "rustowl";
-    name = "rustowl";
 
     src = fetchGit {
       url = "ssh://git@github.com/cordx56/rustowl.git";
       rev = "58fd467581c397a86a9c60faee330ae19195d52e";
+      name = "rustowl";
     };
 
     sourceRoot = "${src.name}/rustowl";
